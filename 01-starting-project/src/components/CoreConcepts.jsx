4b6185975f7375a9bd008b8e832abcbb14070d06
Florian Ruppel <ruppel.florian@gmail.com>
2025-06-15 21:40:13 +0200

    First project

---
+++ 01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,14 @@
+import {CORE_CONCEPTS} from '../data';
+import CoreConcept from './CoreConcept';
+import Section from './Section';
+
+export default function CoreConcepts() {
+    return (
+        <Section title="Time to get started!" id="core-concepts">
+            <ul>
+                {CORE_CONCEPTS.map((conceptItem) =>
+                    <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+            </ul>
+        </Section>
+    )
+}
