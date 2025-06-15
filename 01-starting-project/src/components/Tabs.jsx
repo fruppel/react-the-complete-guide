c2a1f9a6ec82396d3e35606ce57feb88d9394551
Florian Ruppel <ruppel.florian@gmail.com>
2025-06-15 22:02:31 +0200

    Improve components

--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,8 +1,8 @@
-export default function Tabs({children, buttons}) {
+export default function Tabs({children, buttons, ButtonContainer='menu'}) {
     return <>
-        <menu>
+        <ButtonContainer>
             {buttons}
-        </menu>
+        </ButtonContainer>
         {children}
     </>;
 }
