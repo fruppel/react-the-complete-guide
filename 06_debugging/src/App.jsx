a66adf9d2ee042ccf1a6829a1d17ba7256fbb726
Florian Ruppel <ruppel.florian@gmail.com>
2025-08-12 21:44:30 +0200

    Test setup

--- 06_debugging/src/App.jsx
+++ 06_debugging/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
 import Results from './components/Results.jsx';
