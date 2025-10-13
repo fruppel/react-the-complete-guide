16fcb991322ef8fa2495bee9d4d7be2a7806f460
Florian Ruppel <ruppel.florian@gmail.com>
2025-10-13 14:18:49 +0200

    Add timers

--- 08_refs_and_portals/src/components/Player.jsx
+++ 08_refs_and_portals/src/components/Player.jsx
@@ -1,11 +1,22 @@
+import {useState, useRef} from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+
+    let playerName = useRef();
+    let [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+
+    function handleClick() {
+        setEnteredPlayerName(playerName.current.value);
+    }
+
+    return (
+        <section id="player">
+            <h2>Welcome {enteredPlayerName ?? 'Unknown'}!</h2>
+            <p>
+                <input type="text" ref={playerName} />
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
