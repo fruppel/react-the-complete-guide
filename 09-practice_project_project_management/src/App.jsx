2d6a8cf3e381f5e78c496aa4e0fbabdab92f3a72
Florian Ruppel <ruppel.florian@gmail.com>
2025-10-20 12:41:00 +0200

    Add page handling

--- 09-practice_project_project_management/src/App.jsx
+++ 09-practice_project_project_management/src/App.jsx
@@ -1,16 +1,40 @@
+import {useState} from 'react';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
-  return (
-    <>
-        <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar />
-            <NoProjectSelected />
-        </main>
-    </>
-  );
+
+    const [projectsState, setProjectsState] = useState({
+        projects: [],
+        selectedProjectId: undefined,
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            }
+        });
+    }
+
+    let content;
+
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject />;
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+    }
+
+    return (
+        <>
+            <main className="h-screen my-8 flex gap-8">
+                <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+                {content}
+            </main>
+        </>
+    );
 }
 
 export default App;
