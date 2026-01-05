212af61f5bfd85f10792202b4d9217d01daea15a
Florian Ruppel <ruppel.florian@gmail.com>
2026-01-05 14:22:42 +0100

    Improve modal handling

--- 11_side-effects/src/App.jsx
+++ 11_side-effects/src/App.jsx
@@ -11,10 +11,10 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
 const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find(place => place.id === id));
 
 function App() {
-    const modal = useRef();
     const selectedPlace = useRef();
     const [availablePlaces, setAvailablePlaces] = useState([])
     const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -30,12 +30,12 @@ function App() {
 
 
     function handleStartRemovePlace(id) {
-        modal.current.open();
+        setModalIsOpen(true);
         selectedPlace.current = id;
     }
 
     function handleStopRemovePlace() {
-        modal.current.close();
+        setModalIsOpen(false);
     }
 
     function handleSelectPlace(id) {
@@ -58,7 +58,7 @@ function App() {
         setPickedPlaces((prevPickedPlaces) =>
             prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
         );
-        modal.current.close();
+        setModalIsOpen(false);
 
         const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
 
@@ -70,7 +70,10 @@ function App() {
 
     return (
         <>
-            <Modal ref={modal}>
+            <Modal
+                open={modalIsOpen}
+                onClose={handleStopRemovePlace}
+            >
                 <DeleteConfirmation
                     onCancel={handleStopRemovePlace}
                     onConfirm={handleRemovePlace}
